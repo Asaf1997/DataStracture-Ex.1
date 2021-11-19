dc62fe5c8314982710d56d543fd6246aac283397
YofIAsi <asafshilo@mail.tau.ac.il>
2021-11-19 11:15:38 +0200

    initial commit

--- src/tester.java
+++ src/tester.java
@@ -2,6 +2,15 @@ package src;
 
 public class tester {
     public static void main(String[] args) {
+        asafTest();
+        idoTest();
+    }
+
+    public void idoTest(){
+
+    }
+
+    public void asafTest(){
 
     }
 
